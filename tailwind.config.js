2fa1ae04dee13c3be5494c8406ef59a4e2f2e49d
Orlovic982 <Orlovic982@users.noreply.github.com>
2023-10-25 22:35:44 +0000

    deploy: 9b7c68682955b25b0f1cccdae1c912ed314f52c5

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,22 @@
 module.exports = {
-    content: [
-      './_drafts/**/*.html',
-      './_includes/**/*.html',
-      './_layouts/**/*.html',
-      './_posts/*.md',
-      './*.md',
-      './*.html',
-    ],
-    theme: {
-      theme: {
-        extend: {},
-      },
+  content: [
+    './_drafts/**/*.html',
+    './_includes/**/*.html',
+    './_layouts/**/*.html',
+    './_posts/*.md',
+    './*.md',
+    './*.html',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        "gold": "#FDBA74"
+      }
     },
-    plugins: []
-  }
+    fontFamily: {
+      imperialCursive: ['Imperial Script', 'cursive'],
+      abrilFatface: ['Abril Fatface', 'serif']
+    }
+  },
+  plugins: []
+}
